bf84332f3c10b1c9d75f88bdabdee5adb53052b6
Fikri100103 <92206174+Fikri100103@users.noreply.github.com>
2022-03-02 19:34:53 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+    // definisikan variables
+    $nama = "Khairul Fikri";
+    $umur = 19;
+    $berat = 54;
+
+    echo "Nama : " . $nama;
+    echo "<br/>Umur : " . $umur. " tahun";
+    echo "<br/>Berat : " . $berat. " kg";
+
+    echo "<br/>Hello $nama Apa kabar";
+
+?>
